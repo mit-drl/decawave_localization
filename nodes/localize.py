e169158f0324c390ffd359ec83a248c8b5b549e9
Alex Wallar <alex@wallar.me>
2017-02-17 16:28:47 -0500

    Working now

--- nodes/localize.py
+++ nodes/localize.py
@@ -10,8 +10,10 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 from sensor_msgs.msg import Range
 
 NODE_NAME = "decawave_localization"
-POSE_TOPIC = "uwb_pose"
-POSE_COV_TOPIC = "pose_cov"
+POSE_TOPIC_3D = "uwb_pose_3d"
+POSE_TOPIC_2D = "uwb_pose_2d"
+POSE_COV_TOPIC_3D = "pose_cov_3d"
+POSE_COV_TOPIC_2D = "pose_cov_2d"
 ERROR_PC_TOPIC = "error_cloud"
 
 
@@ -19,20 +21,24 @@ class DecaWaveLocalization:
 
     def __init__(self):
         self.frame_id = rospy.get_param("~frame_id", "map")
-        cov_x = rospy.get_param("~cov_x", 0.6)
-        cov_y = rospy.get_param("~cov_y", 0.6)
-        cov_z = rospy.get_param("~cov_z", 0.6)
-        self.cov = self.cov_matrix(cov_x, cov_y, cov_z)
-        self.ps_pub = rospy.Publisher(POSE_TOPIC, PoseStamped, queue_size=1)
-        self.ps_cov_pub = rospy.Publisher(
-            POSE_COV_TOPIC, PoseWithCovarianceStamped, queue_size=1)
-        self.rate = rospy.Rate(rospy.get_param("frequency", 30))
-        self.ps = PoseStamped()
-        self.ps.header.frame_id = self.frame_id
-        self.ps_cov = PoseWithCovarianceStamped()
-        self.ps_cov.header.frame_id = self.frame_id
-        self.ps_cov.pose.covariance = self.cov
-        self.last = None
+        cov_x_2d = rospy.get_param("~cov_x_2d", 0.6)
+        cov_y_2d = rospy.get_param("~cov_y_2d", 0.6)
+        cov_z_2d = rospy.get_param("~cov_z_2d", 0.6)
+        cov_x_3d = rospy.get_param("~cov_x_3d", 0.6)
+        cov_y_3d = rospy.get_param("~cov_y_3d", 0.6)
+        cov_z_3d = rospy.get_param("~cov_z_3d", 0.6)
+        self.two_d_tags = rospy.get_param("~two_d_tags")
+        self.cov_2d = self.cov_matrix(cov_x_2d, cov_y_2d, cov_z_2d)
+        self.cov_3d = self.cov_matrix(cov_x_3d, cov_y_3d, cov_z_3d)
+        self.ps_pub_3d = rospy.Publisher(POSE_TOPIC_3D, PoseStamped, queue_size=1)
+        self.ps_pub_2d = rospy.Publisher(POSE_TOPIC_2D, PoseStamped, queue_size=1)
+        self.ps_cov_pub_3d = rospy.Publisher(
+            POSE_COV_TOPIC_3D, PoseWithCovarianceStamped, queue_size=1)
+        self.ps_cov_pub_2d = rospy.Publisher(
+            POSE_COV_TOPIC_2D, PoseWithCovarianceStamped, queue_size=1)
+        self.rate = rospy.Rate(rospy.get_param("~frequency", 30))
+        self.last_3d = None
+        self.last_2d = None
         self.listener = tf.TransformListener()
         self.tag_range_topics = rospy.get_param("~tag_range_topics")
         self.subs = list()
@@ -41,17 +47,32 @@ class DecaWaveLocalization:
         for topic in self.tag_range_topics:
             self.subs.append(rospy.Subscriber(topic, Range, self.range_cb))
 
-    def find_position(self):
-        if self.last is None:
-            self.last = self.tag_pos.values()[0]
-        res = opt.minimize(self.error, self.last, jac=self.jac, method="SLSQP")
-        self.last = res.x
+    def find_position_3d(self):
+        if self.last_3d is None:
+            self.last_3d = self.tag_pos.values()[0]
+        res = opt.minimize(self.error, self.last_3d, jac=self.jac, method="SLSQP")
+        self.last_3d = res.x
+        return res.x
+
+    def find_position_2d(self):
+        if self.last_2d is None:
+            self.last_2d = self.tag_pos.values()[0][:2]
+        res = opt.minimize(self.error_2d, self.last_2d, jac=self.jac_2d, method="SLSQP")
+        self.last_2d = res.x
         return res.x
 
     def error(self, x):
         err = 0.0
         for tag_id in self.ranges.keys():
             tag = self.tag_pos[tag_id]
+            dist = self.ranges[tag_id]
+            err += pow(pow(dist, 2) - pow(np.linalg.norm(x - tag), 2), 2)
+        return err
+
+    def error_2d(self, x):
+        err = 0.0
+        for tag_id in self.two_d_tags:
+            tag = self.tag_pos[tag_id][:2]
             dist = self.ranges[tag_id]
             err += pow(pow(dist, 2) - pow(np.linalg.norm(x - tag), 2), 2)
         return err
@@ -69,6 +90,17 @@ class DecaWaveLocalization:
             jac_z += err * (tag[2] - x[2])
         return np.array([jac_x, jac_y, jac_z])
 
+    def jac_2d(self, x):
+        jac_x = 0.0
+        jac_y = 0.0
+        for tag_id in self.two_d_tags:
+            tag = self.tag_pos[tag_id][:2]
+            dist = self.ranges[tag_id]
+            err = pow(dist, 2) - pow(np.linalg.norm(x - tag), 2)
+            jac_x += err * (tag[0] - x[0])
+            jac_y += err * (tag[1] - x[1])
+        return np.array([jac_x, jac_y])
+
     def range_cb(self, rng):
         self.ranges[rng.header.frame_id] = rng.range
         try:
@@ -78,18 +110,33 @@ class DecaWaveLocalization:
         except:
             return
 
-        if len(self.tag_pos.values()) == len(self.ranges.values()) \
-                and len(self.tag_pos.keys()) >= 3:
-            pos = self.find_position()
-            x, y, z = pos[0], pos[1], pos[2]
-            self.ps.pose.position.x = x
-            self.ps.pose.position.y = y
-            self.ps.pose.position.z = z
-            self.ps.header.stamp = rospy.get_rostime()
-            self.ps_cov.header.stamp = rospy.get_rostime()
-            self.ps_cov.pose.pose = self.ps.pose
-            self.ps_pub.publish(self.ps)
-            self.ps_cov_pub.publish(self.ps_cov)
+        if len(self.ranges.values()) == 6:
+            pos_3d = self.find_position_3d()
+            pos_2d = self.find_position_2d()
+            self.publish_position(
+                pos_3d, self.ps_pub_3d, self.ps_cov_pub_3d, self.cov_3d)
+            self.publish_position(
+                pos_2d, self.ps_pub_2d, self.ps_cov_pub_2d, self.cov_2d)
+            self.ranges = dict()
+
+    def publish_position(self, pos, ps_pub, ps_cov_pub, cov):
+        x, y = pos[0], pos[1]
+        if len(pos) > 2:
+            z = pos[2]
+        else:
+            z = 0
+        ps = PoseStamped()
+        ps_cov = PoseWithCovarianceStamped()
+        ps.pose.position.x = x
+        ps.pose.position.y = y
+        ps.pose.position.z = z
+        ps.header.frame_id = self.frame_id
+        ps.header.stamp = rospy.get_rostime()
+        ps_cov.header = ps.header
+        ps_cov.pose.pose = ps.pose
+        ps_cov.pose.covariance = cov
+        ps_pub.publish(ps)
+        ps_cov_pub.publish(ps_cov)
 
     def cov_matrix(self, x_cov, y_cov, z_cov):
         return [x_cov, 0, 0, 0, 0, 0,
